ebbf134baeb3f8ef5a71f52fd0ab02f419c22c9e
Luis Felipe <vilella.luisfelipe@gmail.com>
2021-02-22 14:43:47 -0300

    starts frontend project

---
+++ frontend/silobytes/src/stacks/MainStack.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Preload from '../screens/Preload';
+import SignIn from '../screens/SignIn';
+
+const Stack = createStackNavigator();
+
+export default () => (
+    <Stack.Navigator
+        initialRouteName="Preload"
+        screenOptions={{
+            headerShown: false
+        }}
+    >
+        <Stack.Screen name="Preload" component={Preload} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+    </Stack.Navigator>
+);
